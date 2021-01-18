101d0469a0f2c9fa4ded770c10e3aa09ccc30dba
Anbini <codingangelata@gmail.com>
2021-01-18 12:25:58 -0500

    updated to take care of aliens

--- jan2021/interview.py
+++ jan2021/interview.py
@@ -7,14 +7,14 @@ name = input("Whats your name : ")
 
 print ("Thank you")
 
-place = input("where do you live ? ")
+place = input("which planet do you live on ? ")
 
 print ("Great")
 
 if place == 'earth' :
 	print ("Thank you "+name+". we dont offer service at "+place +" or even on any earth places yet.")
 else :
-	print ("Thank you "+name+". Nice to know that you live at "+place +". How May I help you?")
+	print ("Thank you "+name+". Nice to know that you are an alien and live at "+place +". How May I help you?")
 
 
 
